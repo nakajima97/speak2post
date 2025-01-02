656dcd714bab2bafd6f0c17490ba83195ae27774
nakajima97 <kituneudon97@gmail.com>
2025-01-02 16:41:52 +0900

    #3 MUIの導入

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
+
+import { Roboto } from 'next/font/google';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme';
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+});
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -15,7 +26,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.variable}>
+      <AppRouterCacheProvider>
+      <ThemeProvider theme={theme}>
+        {children}
+        </ThemeProvider>
+        </AppRouterCacheProvider>
+        </body>
     </html>
   );
 }
